b68483d8f07352ba146943a7a11d3d8a9f3eb6da
Edgar Gálvez <edgar.galvez@istfo.edu.ec>
2025-11-11 11:58:53 -0500

    Add user authentication with login and logout functionality, and create User CRUD controller

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,6 +6,7 @@ use App\Entity\Carrera;
 use App\Entity\EstudioPosterior;
 use App\Entity\ExperienciaLaboral;
 use App\Entity\Graduado;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -27,10 +28,11 @@ class DashboardController extends AbstractDashboardController {
     public function configureMenuItems(): iterable {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::section('Entities');
+        yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Carreras', 'fas fa-list-check', Carrera::class);
         yield MenuItem::linkToCrud('Graduados', 'fas fa-user-graduate', Graduado::class);
         yield MenuItem::linkToCrud('Experiencia Laboral', 'fas fa-briefcase', ExperienciaLaboral::class);
         yield MenuItem::linkToCrud('Estudio Posterior', 'fas fa-book-open', EstudioPosterior::class);
-        //yield MenuItem::linkToLogout('Logout', 'fa fa-sign-out-alt');
+        yield MenuItem::linkToLogout('Logout', 'fa fa-sign-out-alt');
     }
 }
